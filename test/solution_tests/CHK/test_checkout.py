0e89720dcb821e806c050fd44ae841b7be19aefd
sbx_user1051 <sbx_user1051@169.254.250.249>
2022-05-08 13:00:24 +0000

    Sun May 08 13:00:24 UTC 2022

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -15,10 +15,11 @@ from solutions.CHK import checkout_solution
         ("E", 40),
         ("AB", 80),  # two items
         ("AA", 100),  # A discount
-        ("AAA", 130),  # A discount
+        ("AAA", 130),
         ("AAAA", 180),  # 4 items with discount
         ("AAAAA", 200),
-        ("AAAAAA", 250),  # A discount
+        ("AAAAAA", 250),  # A 5 items discount
+        ("AAAAAAAA", 330),  # A discount
         ("BB", 45),  # B discount
         ("AAABB", 175),
         ("AABB", 145),
@@ -43,4 +44,5 @@ def test_sum(skus: str, expected: int):
 )
 def test_allowed_keys(items, expected):
     assert checkout_solution.all_items_allowed(items) == expected
+
 
