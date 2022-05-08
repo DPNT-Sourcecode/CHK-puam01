e61c9b6c16cfde087c905c5299f010d5d9bc0a5d
sbx_user1051 <sbx_user1051@169.254.250.249>
2022-05-08 09:35:06 +0000

    Sun May 08 09:35:06 UTC 2022

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -7,11 +7,12 @@ from solutions.SUM import sum_solution
     "x, y, expected",
     (
         (1, 2, 3),  # normal case
-        (0, 100, 100),
-        (100, 0, 100),
+        (0, 100, 100),  # boundary 0, 100
+        (100, 0, 100),  # boundary 100, 0
     )
 )
 def test_sum(x: int, y: int, expected: int):
     assert sum_solution.compute(x, y) == expected
+
 
 
