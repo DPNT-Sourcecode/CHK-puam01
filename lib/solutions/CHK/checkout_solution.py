96d57d83696a8158bcdabca122a0d5616b024a97
sbx_user1051 <sbx_user1051@169.254.8.1>
2022-05-08 17:26:20 +0000

    Sun May 08 17:26:20 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,45 +3,9 @@ import operator
 from collections import Counter
 from typing import List, Optional, Tuple
 
-from .models import item, SpecialOfferQuantity, SpecialOfferFree
+from .models import Item, SpecialOfferQuantity, SpecialOfferFree
+from .database import STOCK
 
-
-
-STOCK = {
-    "A": Item(
-        name="A",
-        price=50,
-        special_offer_quantity=[
-            SpecialOfferQuantity(min_quantity=3, offer_price=130),
-            SpecialOfferQuantity(min_quantity=5, offer_price=200),
-        ],
-    ),
-    "B": Item(
-        name="B",
-        price=30,
-        special_offer_quantity=[SpecialOfferQuantity(min_quantity=2, offer_price=45)],
-    ),
-    "C": Item(
-        name="C",
-        price=20,
-    ),
-    "D": Item(
-        name="D",
-        price=15,
-    ),
-    "E": Item(
-        name="E",
-        price=40,
-        special_offer_free=[SpecialOfferFree(min_quantity=2, free_item="B")],
-    ),
-    "F": Item(
-        name="F",
-        price=10,
-        special_offer_free=[
-            SpecialOfferFree(min_quantity=2, basket_quantity=3, free_item="F")
-        ],
-    ),
-}
 
 ALLOWED_ITEMS = set(STOCK.keys())
 PROCESS_ITEMS_ORDER = ("F", "E", "A", "B", "C", "D")
@@ -150,6 +114,7 @@ def checkout(skus: str) -> int:
 
         total += value
     return total
+
 
 
 
