25d6dc18d01cb53c517f8558c36f173e582d4b4e
sbx_user1051 <sbx_user1051@169.254.250.249>
2022-05-08 09:51:49 +0000

    Sun May 08 09:51:49 UTC 2022

--- test/solution_tests/HLO/test_hello.py
+++ test/solution_tests/HLO/test_hello.py
@@ -6,11 +6,12 @@ from solutions.HLO import hello_solution
 @pytest.mark.parametrize(
     "friend_name, expected",
     (
-        ("Craftsman", "Hello, World!"),  # normal case
-        ("Mr. X", "Hello, World!"),
+        ("Craftsman", "Hello, Craftsman!"),  # normal case
+        ("Mr. X", "Hello, Mr. X!"),
     )
 )
 def test_sum(friend_name: str, expected: str):
     assert hello_solution.hello(friend_name) == expected
+
 
 
