0f892c457e0e0d1f5eb3e139e95259454250de56
sbx_user1051 <sbx_user1051@169.254.250.249>
2022-05-08 10:01:39 +0000

    Sun May 08 10:01:39 UTC 2022

---
+++ test/solution_tests/CHK/test_hello.py
@@ -0,0 +1,14 @@
+import pytest
+
+from solutions.HLO import hello_solution
+
+
+@pytest.mark.parametrize(
+    "friend_name, expected",
+    (
+        ("Craftsman", "Hello, Craftsman!"),  # normal case
+        ("Mr. X", "Hello, Mr. X!"),
+    )
+)
+def test_sum(friend_name: str, expected: str):
+    assert hello_solution.hello(friend_name) == expected
