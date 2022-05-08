9ff8603618caa195a35701b83d0db4bde0e56730
sbx_user1051 <sbx_user1051@169.254.250.249>
2022-05-08 09:32:06 +0000

    Sun May 08 09:32:06 UTC 2022

--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -6,10 +6,12 @@ from solutions.SUM import sum_solution
 @pytest.mark.parametrize(
     "x, y, expected",
     (
-        (1, 2, 3),
-        (1, 2, 4),
+        (1, 2, 3),  # normal case
+        (0, 100, 100),
+        (100, 0, 100),
     )
 )
 def test_sum(x: int, y: int, expected: int):
     assert sum_solution.compute(x, y) == expected
+
 
