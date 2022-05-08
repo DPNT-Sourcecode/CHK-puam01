566a6625fe4cbbf7f33a6a54a628abd7d82b8aab
sbx_user1051 <sbx_user1051@169.254.8.1>
2022-05-08 19:37:03 +0000

    Sun May 08 19:37:03 UTC 2022

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -172,13 +172,19 @@ def checkout(skus: str) -> int:
         total_items += item["count"]
         discounted_items = math.floor(total_items / MIN_GROUP_ITEMS)
         discounted_price = discounted_items * GROUP_ITEMS_PRICE
+        ic(discounted_price)
 
         # number of items outside of the discount
         remaining_items = total_items - (discounted_items * MIN_GROUP_ITEMS)
+        ic(remaining_items)
         # initial value, no discount
         value = item["price"] * remaining_items
+        ic(value)
         partial_sum += value + discounted_price
+        ic(partial_sum)
 
+    total += partial_sum
     return total
+
 
 
